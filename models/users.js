63a376c4ba7eb2c79cec8d704e78ff1224cba69a
Pavel <neo2318@gmail.com>
2013-12-14 20:04:42 -0500

    optimizando el modelo user

--- models/users.js
+++ models/users.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-	user:      { type: String },
-	name:      { type: String },
-	last_name: { type: String },
-	email:     { type: String },
+	user:      { type: String, required: true, unique: true },
+	name:      { type: String, required: true },
+	last_name: { type: String, required: true },
+	email:     { type: String, required: true, unique: true },
 	company:   { type: String },
 	phone:     { type: String },
 	latitude:  { type: String },
@@ -13,4 +13,55 @@ var userSchema = new Schema({
 });
 
 
-module.exports = mongoose.model('user', userSchema);
+var User = mongoose.model('user', userSchema);
+
+User.findAll = function(callback){
+	this.find(function(err, users){
+		if(err)
+			callback(err, null);
+		else
+			callback(err, users);
+	});
+};
+
+User.findByEmail = function(_email, callback){
+	this.findOne({email: _email}, function(err, user){
+		if(err)
+			callback(err, null);
+		else
+			callback(err, user);
+	});
+};
+
+User.newUser = function(_users, callback){
+	var user = new User(_users);
+
+    user.save(function(err, data) {
+		if(err)
+			callback(err, null);
+		else
+			callback(err, data);
+    });
+};
+
+User.updateUser = function(_email, _users, callback){
+
+	this.update({email: _email}, _users, {multi: false}, function (err, count) {
+		if(err)
+			callback(err, null);
+		else
+			callback(err, count);
+	});
+};
+
+User.removeUser = function(_email, callback){
+	this.remove({email: _email}, function (err, count) {
+		if(err)
+			callback(err, null);
+		else
+			callback(err, count);
+	});
+
+};
+
+module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -4,56 +4,64 @@ module.exports = function(app) {
   var User = require('../models/users.js');
 
   findAllUsers = function(req, res) {
-    return User.find(function(err, users) {
-      if(!err) {
-        console.log('GET /users');
-        res.json(users);
-      } else {
-        console.log('ERROR: ' + err);
-      }
+    User.findAll(function(err, users){
+        if(!err){
+            res.json(users);
+        }else{
+            res.json({'error':'An error has occurred'});
+        }
     });
   };
 
   findByEmail = function(req, res) {
-    return User.find({email: req.params.email}, function(err, user) {
-      if(!err) {
-        console.log('GET /user/' + req.params.email);
-        res.json(user);
-      } else {
-        console.log('ERROR: ' + err);
-      }
+    User.findByEmail(req.params.email, function(err, user){
+        if(!err){
+            res.json(user);
+        }else{
+            res.json({'error':'An error has occurred'});
+        }
     });
   };
 
   addUser = function(req, res) {
-    console.log('POST');
-    console.log(req.body);
+    var user_data = {
+            user:      req.body.user,
+            name:      req.body.name,
+            last_name: req.body.last_name,
+            email:     req.body.email,
+            company:   req.body.company,
+            phone:     req.body.phone,
+            latitude:  req.body.latitude,
+            longitude: req.body.longitude
+    };
 
-    var user = new User({
-      user:      req.body.user,
-      name:      req.body.name,
-      last_name: req.body.last_name,
-      email:     req.body.email,
-      company:   req.body.company,
-      phone:     req.body.phone,
-      latitude:  req.body.latitude,
-      longitude: req.body.longitude
-    });
-
-    return user.save(function(err, data) {
-      return res.json(data);
+    User.newUser(user_data, function(err, user){
+        if(!err){
+            res.json(user);
+        }else{
+            res.json({'error':'An error has occurred'});
+        }
     });
   };
 
   updateUser = function(req, res) {
-    return User.update({email: req.params.email}, req.body, {multi: false}, function (err, count) {
-        return res.json({count: count});
+    User.updateUser(req.params.email, req.body, function(err, count){
+        if(!err){
+            res.json({count: count});
+        }else{
+            res.json({'error':'An error has occurred'});
+        }
     });
+
   };
 
-  deleteUser = function(){
-    return User.remove({email: req.params.email}, function (err, count) {
-        return res.json({count: count});
+  deleteUser = function(req, res){
+    User.removeUser(req.params.email, function(err, count){
+        if(!err){
+            res.json({count: count});
+        }else{
+            res.json({'error':'An error has occurred'});
+        }
     });
   };
 
